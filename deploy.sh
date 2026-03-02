9d2ab93810002458bbf4ab7d84ad96b4ff32b200
Kevin Sigey <sigeykevin@gmail.com>
2026-03-02 10:20:47 -0600

    new updates

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # Usage: ./deploy.sh <workflow_id> <path_to_json_file>
 
 N8N_URL="http://localhost:5678/api/v1/workflows"
-API_KEY="PASTE_YOUR_N8N_API_KEY_HERE"
+API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0N2Q2ZjRmMi04M2Y0LTQ5MTMtODZiNi0xODRiNzk2ODcyNzEiLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwianRpIjoiNDRhYWY2ZDYtMTdjMi00MzJmLWJlOWItNjA4NDdhNTk2YTUzIiwiaWF0IjoxNzcyNDIyNTcxfQ.udAx7u5mcbFIJBT7fWFBriEYp8AjAubvsrSfKs1uX_M"
 
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Usage: ./deploy.sh <workflow_id> <path_to_json_file>"
